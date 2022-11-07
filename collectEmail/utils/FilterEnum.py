b6380f8745083637d7853b6d2c54c3aaa963f972
ElardHuaraca <elard.sd.9@gmail.com>
2022-11-07 08:38:23 -0500

    Update const filter

--- collectEmail/utils/FilterEnum.py
+++ collectEmail/utils/FilterEnum.py
@@ -1,6 +1,6 @@
 class FilterEnum(enumerate):
     GROUP = ['apagado', 'migrado', 'de_baja', 'pendiente baja',
-             'de baja', 'desactivado_temporalmente', 'test', 'migrado', 'historicos', 'migra_veeam']
+             'de baja', 'desactivado_temporalmente', 'test', 'migrado', 'historico', 'migra_veeam']
     SPECIFICATION = ['dia', 'dem', 'demanda', 'crq', 'rfc']
     STATUS = ['aborted', 'failed', 'completed/failures']
     MALFORMED = ['<= /font>', '=font>', '< /font>',
