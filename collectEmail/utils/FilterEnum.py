cb881630abceca4b8e8e67a93e2f4037c1e59144
ElardHuaraca <elard.sd.9@gmail.com>
2022-11-08 22:49:45 -0500

    Update FilterEnum for GROUP

--- collectEmail/utils/FilterEnum.py
+++ collectEmail/utils/FilterEnum.py
@@ -1,5 +1,5 @@
 class FilterEnum(enumerate):
-    GROUP = ['apagado', 'migrado', 'de_baja', 'pendiente baja',
+    GROUP = ['apagado', 'migrado', 'baja', 'de_baja', 'pendiente baja',
              'de baja', 'desactivado_temporalmente', 'test', 'migrado', 'historico', 'migra_veeam']
     SPECIFICATION = ['dia', 'dem', 'demanda', 'crq', 'rfc']
     STATUS = ['aborted', 'failed', 'completed/failures']
